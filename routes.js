c39ba060a70b8d0dbde6591018f4d11fa14194a5
Isaacarzu <isaacarzu@ucwv.edu>
2024-10-30 21:49:02 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,53 +1,30 @@
-const express = require("express");
+const router = require('express').Router();
 
-const router = express.Router();
-const { getConnectedClient } = require("./database");
-const { objectId } = require("mongodb");
-
-const getCollection = () => {
-    const client = getConnectedClient();
-    const collection = client.db("todosdb").collection("todos");
-    return collection
-}
+const todoItemModel = require('../models/todoItems');
 
 
-// GET / todos
-router.get("/todos", async (req, res) => {
-   const collection = getCollection();
-   const todos = await collection.find({}).toArray();
 
-    res.status(200).json({ mssg: "GET REQUEST TO /api/todos"});
-});
+router.post('/api/item', async (req, res)=>{
+    try{
+        const newItem = new todoItemModel({
+          item: req.body.item
+        })
 
-// POST / todos
-router.post("/todos", async (req, res) => {
-    const collection = getCollection();
-    const { todos } = req.body;
+        const saveItem = await newItem.save()
+        res.status(200).json(saveItem);
+    }catch(err){
+        res.json(err);
+    }
+})
 
-    const newTodo = await collection.insertOne({ todos, status: false});
+router.get('/api/items', async (req, res)=>{
+    try{
+        const allTodoItems = await todoItemModel.find({});
+        res.status(200).json(allTodoItems)
+    }catch(err){
+        res.json(err);
+    }
+})
+
+  
     
-    res.status(201).json({  todos, status: false, _id: newTodo. insertedId });
-});
-
-// DELETE /todos/ :id
-router.delete("/todos/:id", async (req, res) => {
-    const collection = getCollection();
-    const _id = new  ObjectId(req.params.id);
-
-    const deletedTodo = await collection.deleteOne({ _id });
-
-    res.status(200).json(deletedTodo);
-});
-
-// PUT /todos/ :id
-router.put("/todos/:id", async (req, res) => {
-    const collection = getCollection();
-    const _id = new  ObjectId(req.params.id);
-    const { status } = req.body;
-
-    const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
-
-    res.status(200).json( updatedTodo );
-});
-
-module.exports = router;
